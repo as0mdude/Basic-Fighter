35cddb9a9ffc24f00104c83ea915c777ef6d824c
asom <86374869+as0mdude@users.noreply.github.com>
2024-06-10 14:35:57 -0700

    collision and crotching

--- fighter/src/Scenes/fighter.js
+++ fighter/src/Scenes/fighter.js
@@ -1,6 +1,6 @@
 class Fighter extends Phaser.Scene {
     constructor() {
-        super("monsterScene");
+        super("FighterScene");
         this.my = {sprite: {}};  // Create an object to hold sprite bindings
     }
 
@@ -11,25 +11,23 @@ class Fighter extends Phaser.Scene {
     }
 
     create() {
-        this.player1 = this.add.graphics();
+        this.player1 = new Phaser.Geom.Rectangle(100, 480, 50, 120);
+        this.player1Graphics = this.add.graphics();
 
         // Set the fill style of the rectangle
-        this.player1.fillStyle(0xFF0000); // Red color
+        this.player1Graphics.fillStyle(0xFF0000); // Red color
     
         // Draw the rectangle
-        this.player1.fillRect(0, 0, 50, 120); // x, y, width, height
-        this.player1.y = 470;
-        this.player1.x = 100;
+        this.player1Graphics.fillRectShape(this.player1);
 
-        this.player2 = this.add.graphics();
+        this.player2 = new Phaser.Geom.Rectangle(600, 480, 50, 120);
+        this.player2Graphics = this.add.graphics();
 
         // Set the fill style of the rectangle
-        this.player2.fillStyle(0x0000FF); // Blue color
+        this.player2Graphics.fillStyle(0x0000FF); // Blue color
     
         // Draw the rectangle
-        this.player2.fillRect(0, 0, 50, 120); // x, y, width, height
-        this.player2.y = 470;
-        this.player2.x = 600;
+        this.player2Graphics.fillRectShape(this.player2);
 
         this.cursors = this.input.keyboard.addKeys({
             left: Phaser.Input.Keyboard.KeyCodes.LEFT,
@@ -38,36 +36,96 @@ class Fighter extends Phaser.Scene {
             down: Phaser.Input.Keyboard.KeyCodes.DOWN
         });
 
+        this.player1crouch = false;
+        this.player2crouch = false;
 
         console.log("test");
     }
 
     update() {
-        if (this.collides(this.player1, this.player2)){
+
+        if (this.collides(this.player1, this.player2)) {
             console.log("Collides");
         }
         
-        if (this.input.keyboard.addKey('A').isDown) {
+
+        if (this.input.keyboard.addKey('A').isDown ) {
+
             this.player1.x -= 2.5;
+            this.player1Graphics.clear();
+            this.player1Graphics.fillStyle(0xFF0000); // Red color
+            this.player1Graphics.fillRectShape(this.player1);
         }
-
-        if (this.input.keyboard.addKey('D').isDown) {
+    
+        if (this.input.keyboard.addKey('D').isDown && !this.collides(this.player1, this.player2)) {
             this.player1.x += 2.5;
+            this.player1Graphics.clear();
+            this.player1Graphics.fillStyle(0xFF0000); // Red color
+            this.player1Graphics.fillRectShape(this.player1);
         }
 
-        if (this.cursors.left.isDown) {
+        if (this.input.keyboard.addKey('S').isDown && !this.player1crouch) {
+            this.player1.height /= 2;
+            this.player1.y += 60
+            this.player1crouch = true;
+            this.player1Graphics.clear();
+            this.player1Graphics.fillStyle(0xFF0000); // Red color
+            this.player1Graphics.fillRectShape(this.player1);
+        } else if (!this.input.keyboard.addKey('S').isDown && this.player1crouch) {
+            this.player1.height *= 2;
+            this.player1.y -= 60
+            this.player1crouch = false;
+            this.player1Graphics.clear();
+            this.player1Graphics.fillStyle(0xFF0000); // Red color
+            this.player1Graphics.fillRectShape(this.player1);
+        }
+
+        
+    
+        if (this.cursors.left.isDown && !(this.collides(this.player1, this.player2))) {
             this.player2.x -= 2.5;
+            this.player2Graphics.clear();
+            this.player2Graphics.fillStyle(0x0000FF); // Blue color
+            this.player2Graphics.fillRectShape(this.player2);
         }
     
         if (this.cursors.right.isDown) {
             this.player2.x += 2.5;
+            this.player2Graphics.clear();
+            this.player2Graphics.fillStyle(0x0000FF); // Blue color
+            this.player2Graphics.fillRectShape(this.player2);
         }
+
+        if (this.cursors.down.isDown && !this.player2crouch) {
+            this.player2.height /= 2;
+            this.player2.y += 60
+            this.player2crouch = true;
+            this.player2Graphics.clear();
+            this.player2Graphics.fillStyle(0x0000FF); // Blue color
+            this.player2Graphics.fillRectShape(this.player2);
+        } else if (!this.cursors.down.isDown && this.player2crouch) {
+            this.player2.height *= 2;
+            this.player2.y -= 60
+            this.player2crouch = false;
+            this.player2Graphics.clear();
+            this.player2Graphics.fillStyle(0x0000FF); // Blue color
+            this.player2Graphics.fillRectShape(this.player2);
+        }
+
+        //console.log(this.player2.height)
     }
 
+    
+
     collides(a, b) {
-        if (Math.abs(a.x - b.x) > (a.displayWidth/2 + b.displayWidth/2)) return false;
-        if (Math.abs(a.y - b.y) > (a.displayHeight/2 + b.displayHeight/2)) return false;
-        return true;
+
+        
+        return (
+            a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y
+        );
     }
 
 
